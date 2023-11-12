ed4e62c44d32968c764c7094aa13f621c7b58956
Javier <javier.rodriguez@caci.com>
2023-11-12 00:53:15 -0500

    Added navbar

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 
 
 @NgModule({
   declarations: [
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    NavbarComponent
+  ],
+  exports: [
+    NavbarComponent
   ],
   imports: [
     CommonModule
