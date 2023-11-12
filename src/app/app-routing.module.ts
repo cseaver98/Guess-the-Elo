205299194fe3f79127991ce2afa01c32e4bef7fe
seavercj <seavercj@dukes.jmu.edu>
2023-11-12 00:14:02 -0500

    route to play page implemented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {HomePageComponent} from "./feature/home-page/home-page.component";
 import {PageNotFoundComponent} from "./core/page-not-found/page-not-found.component";
+import {featureRoutes} from './feature/feature-routing.module';
 
 const routes: Routes = [
-  { path: '', component: HomePageComponent },
+  { path: 'home', component: HomePageComponent },
+  ...featureRoutes,
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent }
 ];
 
--- src/app/feature/feature.module.ts
+++ src/app/feature/feature.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './home-page/home-page.component';
+import { PlayPageComponent } from './play-page/play-page.component';
+import { FeatureRoutingModule } from './feature-routing.module';
 
 
 
 @NgModule({
   declarations: [
-    HomePageComponent
+    HomePageComponent,
+    PlayPageComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FeatureRoutingModule
   ]
 })
 export class FeatureModule { }
