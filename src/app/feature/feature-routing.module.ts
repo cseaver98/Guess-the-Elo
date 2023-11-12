be9ea464d37dc180795cf765f7d97c08c7112b55
seavercj <seavercj@dukes.jmu.edu>
2023-11-12 00:15:51 -0500

    wtf

---
+++ src/app/feature/feature-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { PlayPageComponent } from './play-page/play-page.component';
+
+export const featureRoutes: Routes = [
+  { path: 'play-page', component: PlayPageComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(featureRoutes)],
+  exports: [RouterModule]
+})
+export class FeatureRoutingModule { }
