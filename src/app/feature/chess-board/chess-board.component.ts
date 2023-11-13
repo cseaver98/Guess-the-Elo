a2b66b03ce4a146a9d4d3d19363681f800bc2d2e
Javier Rodriguez <stephanrodz@gmail.com>
2023-11-12 19:25:20 -0500

    Added Buttons

--- src/app/feature/chess-board/chess-board.component.ts
+++ src/app/feature/chess-board/chess-board.component.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
-import {NgxChessBoardService} from 'ngx-chess-board';
+import {Component, ViewChild} from '@angular/core';
+import {NgxChessBoardView} from 'ngx-chess-board';
+
 @Component({
   selector: 'app-chess-board',
   templateUrl: './chess-board.component.html'
 })
 export class ChessBoardComponent {
-  constructor(private _: NgxChessBoardService) { }
+  @ViewChild('board', {static: false}) board!: NgxChessBoardView;
+
+  resetBoard() {
+    this.board.reset();
+  }
+
+  undoMove() {
+    this.board.undo()
+  }
 }
--- src/app/feature/play-page/play-page.component.ts
+++ src/app/feature/play-page/play-page.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {ChessBoardComponent} from "../chess-board/chess-board.component";
 
 @Component({
   selector: 'app-play-page',
-  templateUrl: './play-page.component.html',
-  styleUrls: ['./play-page.component.css']
+  templateUrl: './play-page.component.html'
 })
 export class PlayPageComponent {
+  @ViewChild(ChessBoardComponent) chessboard!: ChessBoardComponent;
 
+  resetBoard() {
+    this.chessboard.resetBoard();
+  }
+
+  undo() {
+    this.chessboard.undoMove();
+  }
 }
