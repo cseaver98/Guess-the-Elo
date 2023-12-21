bb57a4deb8a95b606a1b3292c2981589838803d4
Javier Rodriguez <stephanrodz@gmail.com>
2023-12-20 19:04:14 -0500

    Some screen refactors and eval bar

--- src/app/feature/components/evaluation-bar/evaluation-bar.component.ts
+++ src/app/feature/components/evaluation-bar/evaluation-bar.component.ts
@@ -1,5 +1,4 @@
-import { Component } from '@angular/core';
-import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { Component, Input } from '@angular/core';
 
 /**
  * @title Determinate progress-bar
@@ -8,6 +7,17 @@ import { MatProgressBarModule } from '@angular/material/progress-bar';
   selector: 'app-evaluation-bar',
   templateUrl: './evaluation-bar.component.html',
   standalone: true,
-  imports: [MatProgressBarModule],
 })
-export class EvaluationBarComponent {}
+export class EvaluationBarComponent {
+  @Input()
+  evaluation: number = 0;
+
+  cap: number = 5;
+
+  protected percentage: number = ((this.getPercentage(this.evaluation) + this.cap) / (this.cap * 2)) * 100;
+
+  getPercentage(val: number): number {
+    val = val * -1;
+    return Math.min(Math.max(val, (-1 * this.cap)), this.cap);
+  }
+}
