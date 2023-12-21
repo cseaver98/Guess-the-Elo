cb43738f7099c1dbafe1a0b1ac8b2c9e2b8ca3c6
Javier Rodriguez <stephanrodz@gmail.com>
2023-12-20 19:49:48 -0500

    Fixed routing without modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PageNotFoundComponent } from './core/components/page-not-found/page-not-found.component';
 import { HomePageComponent } from './feature/components/home-page/home-page.component';
+import {PlayPageComponent} from "./feature/components/play-page/play-page.component";
 
 const routes: Routes = [
   { path: 'home', component: HomePageComponent },
-  {
-    path: 'play-page',
-    loadChildren: () =>
-      import('./feature/feature.module').then((_x) => _x.FeatureModule),
-  },
+  { path: 'play-page', component: PlayPageComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent },
 ];
